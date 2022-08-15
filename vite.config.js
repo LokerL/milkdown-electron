ec1434ad27cc8eaec134a5dd4afad7ff01cb3275
LokerL <806076693@qq.com>
2022-08-15 11:48:23 +0800

    初始化仓库electron-vite-vue-js

---
+++ vite.config.js
@@ -0,0 +1,63 @@
+import { rmSync } from "fs";
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import electron from "vite-plugin-electron";
+import pkg from "./package.json";
+
+rmSync("dist", { recursive: true, force: true }); // v14.14.0
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    electron({
+      main: {
+        entry: "electron/main/index.js",
+        vite: withDebug({
+          build: {
+            outDir: "dist/electron/main",
+          },
+        }),
+      },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          index: path.join(__dirname, "electron/preload/index.js"),
+        },
+        vite: {
+          build: {
+            // For Debug
+            sourcemap: "inline",
+            outDir: "dist/electron/preload",
+          },
+        },
+      },
+      // Enables use of Node.js API in the Renderer-process
+      // https://github.com/electron-vite/vite-plugin-electron/tree/main/packages/electron-renderer#electron-renderervite-serve
+      renderer: {},
+    }),
+  ],
+  server: {
+    host: pkg.env.VITE_DEV_SERVER_HOST,
+    port: pkg.env.VITE_DEV_SERVER_PORT,
+  },
+});
+
+function withDebug(config) {
+  if (process.env.VSCODE_DEBUG) {
+    if (!config.build) config.build = {};
+    config.build.sourcemap = true;
+    config.plugins = (config.plugins || []).concat({
+      name: "electron-vite-debug",
+      configResolved(config) {
+        const index = config.plugins.findIndex(
+          (p) => p.name === "electron-main-watcher"
+        );
+        // At present, Vite can only modify plugins in configResolved hook.
+        config.plugins.splice(index, 1);
+      },
+    });
+  }
+  return config;
+}
